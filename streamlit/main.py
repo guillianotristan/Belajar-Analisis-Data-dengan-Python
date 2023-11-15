a311412b975cfcb87ebea96f6bbe7135b4ab9853
guillianotristan <138111715+guillianotristan@users.noreply.github.com>
2023-11-15 14:44:47 +0700

    Ma

--- streamlit/main.py
+++ streamlit/main.py
@@ -28,7 +28,7 @@ max_date = all_df["order_approved_at"].max()
 # Sidebar
 with st.sidebar:
     # Title
-    st.title("Hanif Al Irsyad")
+    st.title("Guilliano Tristan Anthony Stevenson")
 
     # Logo Image
     st.image("./streamlit/gcl.png")
@@ -211,5 +211,5 @@ with tab3:
     with st.expander("See Explanation"):
         st.write('Sesuai dengan grafik yang sudah dibuat, ada lebih banyak pelanggan di bagian tenggara dan selatan. Informasi lainnya, ada lebih banyak pelanggan di kota-kota yang merupakan ibu kota (São Paulo, Rio de Janeiro, Porto Alegre, dan lainnya).')
 
-st.caption('Copyright (C) Hanif Al Irsyad 2023')
+st.caption('Copyright (C) Guilliano Tristan Anthony Stevenson 2023')
 
